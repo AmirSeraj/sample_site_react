a234717eef3fd5e64bc938ef3055a9512b52134a
amirseraj <amirseraj.iliyas@yahoo.com>
2023-11-19 00:52:35 +0330

    second commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Home from './components/Home';
-import Header from './components/Header';
-import { ThemeProvider, createTheme, CssBaseline } from '@mui/material';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import Header from "./components/Header";
+import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
 
 function App() {
-
-  const theme = createTheme({
-    palette: {
-      type: 'dark'
-    }
-  })
+  // const theme = createTheme({
+  //   palette: {
+  //     type: "dark",
+  //   },
+  // });
   return (
-    <ThemeProvider>
-      <CssBaseline />
-      <BrowserRouter>
-        <div className="app">
-          <header>
-            <Header />
-          </header>
-          <main>
-            <Routes>
-              <Route path='/' element={<Home />} />
-            </Routes>
-          </main>
-        </div>
-      </BrowserRouter>
-    </ThemeProvider>
+    // <ThemeProvider theme={theme}>
+    //   <CssBaseline />
+      <div className="app">
+        <header>
+          <Header />
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </main>
+      </div>
+    // </ThemeProvider>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,43 +1,193 @@
-import { Box, Container, List, ListItem, ListItemButton, Typography, } from "@mui/material";
+import {
+  Box,
+  Container,
+  IconButton,
+  Menu,
+  MenuItem,
+  Tooltip,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useState } from "react";
 
 const Header = () => {
+  const [anchorE1, setAnchorE1] = useState(null);
+  const open = Boolean(anchorE1);
+  const handleCLick = (e) => {
+    setAnchorE1(e.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorE1(null);
+  };
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  console.log(isMobile);
 
-    return (
-        <Container>
-            <Box display="flex" alignItems="center" justifyContent="space-between">
-                <List style={{ display: "flex", flexDirection: "row", justifyContent: "center" }}>
-                    <ListItem disablePadding>
-                        <ListItemButton sx={{ justifyContent: 'center' }}>
-                            بلاگ
-                        </ListItemButton>
-                    </ListItem>
-                    <ListItem>
-                        <ListItemButton sx={{ justifyContent: 'center' }}>
-                            جزئیات پروژه
-                        </ListItemButton>
-                    </ListItem>
-                    <ListItem>
-                        <ListItemButton sx={{ justifyContent: 'center' }}>
-                            پورتفلو
-                        </ListItemButton>
-                    </ListItem>
-                    <ListItem>
-                        <ListItemButton sx={{ justifyContent: 'center' }}>
-                            درباره ما
-                        </ListItemButton>
-                    </ListItem>
-                    <ListItem>
-                        <ListItemButton sx={{ justifyContent: 'center' }}>
-                            خانه
-                        </ListItemButton>
-                    </ListItem>
-                </List>
-                <Box>
-                    <Typography variant="h1" fontSize="1.5rem" fontWeight="bold">Neve</Typography>
-                </Box>
-            </Box>
-        </Container>
-    )
-}
+  return (
+    <Container sx={{ height: "100%" }}>
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+        height="100%"
+      >
+        <Tooltip
+          title="Account Setting"
+          sx={{ display: isMobile ? "block" : "none" }}
+        >
+          <IconButton
+            onClick={handleCLick}
+            size="small"
+            aria-controls={open ? "account-menu" : undefined}
+            aria-haspopup="tree"
+            aria-expanded={open ? "true" : undefined}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Tooltip>
+        <Menu
+          dir="rtl"
+          anchorEl={anchorE1}
+          id="account-menu"
+          open={open}
+          onClose={handleClose}
+          onClick={handleCLick}
+          PaperProps={{
+            elevation: 0,
+            sx: {
+              overflow: "visible",
+              filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+              mt: 1.5,
+              "& .MuiAvatar-root": {
+                width: 32,
+                height: 32,
+                ml: -0.5,
+                mr: 1,
+              },
+              "&:before": {
+                content: '""',
+                display: "block",
+                position: "absolute",
+                top: 0,
+                right: 14,
+                width: 10,
+                height: 10,
+                bgcolor: "background.paper",
+                transform: "translateY(-50%) rotate(45deg)",
+                zIndex: 0,
+              },
+            },
+          }}
+          transformOrigin={{ horizontal: "right", vertical: "top" }}
+          anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+        >
+          <MenuItem onClick={handleClose}>بلاگ</MenuItem>
+          <MenuItem onClick={handleClose}>جزئیات پروژه</MenuItem>
+          <MenuItem onClick={handleClose}>پورتفلو</MenuItem>
+          <MenuItem onClick={handleClose}> درباره ما</MenuItem>
+          <MenuItem onClick={handleClose}>خانه</MenuItem>
+        </Menu>
+        {/* <Box display={{ sm: "none", xs: "flex" }}>
+          <MenuIcon />
+        </Box> */}
+        <Box
+          display={{ sm: "flex", xs: "none" }}
+          alignItems="center"
+          gap={2}
+          height="100%"
+        >
+          <Box
+            sx={{
+              "&:hover": {
+                borderBottom: "2px solid #000",
+                backgroundColor: "#ccc",
+              },
+            }}
+            color="#000"
+            component={Link}
+            p="8px"
+            display="flex"
+            height="76%"
+            alignItems="center"
+          >
+            بلاگ
+          </Box>
+          <Box
+            sx={{
+              "&:hover": {
+                borderBottom: "2px solid #000",
+                backgroundColor: "#ccc",
+              },
+            }}
+            color="#000"
+            component={Link}
+            p="8px"
+            display="flex"
+            height="76%"
+            alignItems="center"
+          >
+            جزئیات پروژه
+          </Box>
+          <Box
+            sx={{
+              "&:hover": {
+                borderBottom: "2px solid #000",
+                backgroundColor: "#ccc",
+              },
+            }}
+            color="#000"
+            component={Link}
+            p="8px"
+            display="flex"
+            height="76%"
+            alignItems="center"
+          >
+            پورتفلو
+          </Box>
+          <Box
+            sx={{
+              "&:hover": {
+                borderBottom: "2px solid #000",
+                backgroundColor: "#ccc",
+              },
+            }}
+            color="#000"
+            component={Link}
+            p="8px"
+            display="flex"
+            height="76%"
+            alignItems="center"
+          >
+            درباره ما
+          </Box>
+          <Box
+            sx={{
+              "&:hover": {
+                borderBottom: "2px solid #000",
+                backgroundColor: "#ccc",
+              },
+            }}
+            color="#000"
+            component={Link}
+            p="8px"
+            display="flex"
+            height="76%"
+            alignItems="center"
+          >
+            خانه
+          </Box>
+        </Box>
+        <Box>
+          <Typography variant="h1" fontSize="1.5rem" fontWeight="bold">
+            Neve
+          </Typography>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
 
 export default Header;
